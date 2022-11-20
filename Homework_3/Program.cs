911a8d166be4f12613c1a8cbe5d83591dda97c3a
DARYA.BARYSAVETS <darya.barysavets@intapp.com>
2022-11-20 21:04:40 +0200

    HW6

--- Homework_3/Program.cs
+++ Homework_3/Program.cs
@@ -1,4 +1,5 @@
-﻿using ClassLibrary_Employee;
+﻿using ClassLibrary_Human;
+using Client;
 using System;
 using System.Security.AccessControl;
 
@@ -8,29 +9,29 @@ namespace Company
     {
         static void Main()
         {
-            Employee[] persons = new Employee[4];
-            persons[0] = new Employee("Dasha", "Borisovets", "female", 30, "QA");
-            persons[1] = new Employee("Masha", "Fedorova", "female", 31, "BA");
+            Employee employee = new Employee("QA", "Dasha");
+            Employee employee1 = new Employee("Devops", 50);
+            employee.PrintShortInfo();
+            Human[] persons = new Human[4];
+            persons[0] = new Human("Dasha", "Borisovets", "female", 30 );
+            persons[1] = new Human("Masha", "Fedorova", "female", 31 );
 
-            persons[2] = new Employee();
-            persons[2].name = "Pasha";
-            persons[2].lastName = "Semashko";
+            persons[2] = new Human();
+            persons[2].Name = "Pasha";
+            persons[2].LastName = "Semashko";
             persons[2].Gender = "male";
             persons[2].Age = 32;
-            persons[2].Position = "PM";
 
-            persons[3] = new Employee("Oleg", "Ivanov");
+            persons[3] = new Employee("QA", 50);
             persons[3].Age = 40;
-            persons[3].Position = "QA";
             persons[3].Gender = "male";
+            persons[3].Name = "Ali";
+            persons[3].LastName = "Baba";
 
             foreach (Employee e in persons)
             {
-                e.PrintFullInfo();
+                e.PrintShortInfo();
             }
-            persons[3].Work();
-
-            persons[2].PrintShortInfo();
         }
     }
 }
